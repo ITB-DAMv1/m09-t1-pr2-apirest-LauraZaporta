ccb62ed01c236bc5c87d97828653d29757f6a32a
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-19 23:31:07 +0200

    Add project files.

---
+++ API/Data/ApplicationDbContext.cs
@@ -0,0 +1,35 @@
+﻿using API.Model;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class ApplicationDbContext : IdentityDbContext<User> // Fa una taula d'usuaris sense necessitat d'un DbSet
+    {
+        public DbSet<Game> Games { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+            string connString = configuration.GetConnectionString("DefaultConnection");
+            optionsBuilder.UseSqlServer(connString);
+        }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Game>()
+                .HasMany(g => g.UsersWhoVoted)
+                .WithMany(u => u.VotedGames);
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.VotedGames)
+                .WithMany(g => g.UsersWhoVoted);
+        }
+    }
+}
