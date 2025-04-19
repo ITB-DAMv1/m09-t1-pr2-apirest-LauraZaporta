b9617c2c401f11e5e0ec8f59828568be11538f28
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-19 23:55:22 +0200

    Login function and create token added

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -2,6 +2,10 @@
 using Microsoft.AspNetCore.Mvc;
 using API.Model;
 using API.DTOs;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace API.Controllers
 {
@@ -48,6 +52,62 @@ namespace API.Controllers
                 return Ok("Usuari registrat");
 
             return BadRequest(resultat.Errors);
+        }
+
+        // Login
+        // -----
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDTO model)
+        {
+            //Certifiquem que el mail existeix
+            var usuari = await _userManager.FindByEmailAsync(model.Email);
+            if (usuari == null || !await _userManager.CheckPasswordAsync(usuari, model.Password))
+                return Unauthorized("Mail o contrasenya erronis");
+
+            var claims = new List<Claim>()
+            {
+                new Claim(ClaimTypes.Name, usuari.UserName),
+                new Claim(ClaimTypes.NameIdentifier, usuari.Id.ToString())
+            };
+
+            //Adquirim els rols de l'usuari. Pot tenir mes d'un. En aquest cas 1.
+            var roles = await _userManager.GetRolesAsync(usuari);
+
+            if (roles != null && roles.Count > 0)
+            {
+                foreach (var rol in roles)
+                {
+                    claims.Add(new Claim(ClaimTypes.Role, rol));
+                }
+            }
+
+            return Ok(CreateToken(claims.ToArray()));
+        }
+
+        private string CreateToken(Claim[] claims)
+        {
+            // Carreguem les dades des del appsettings.json
+            var jwtConfig = _configuration.GetSection("JwtSettings");
+            var secretKey = jwtConfig["Key"];
+            var issuer = jwtConfig["Issuer"];
+            var audience = jwtConfig["Audience"];
+            var expirationMinutes = int.Parse(jwtConfig["ExpirationMinutes"]);
+
+            // Creem la clau i les credencials de signatura
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secretKey));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            // Construcció del token
+            var token = new JwtSecurityToken(
+                issuer: issuer,
+                audience: audience,
+                claims: claims,
+                expires: DateTime.UtcNow.AddMinutes(expirationMinutes),
+                signingCredentials: creds
+            );
+
+            // Retornem el token serialitzat
+            return new JwtSecurityTokenHandler().WriteToken(token);
         }
     }
 }
