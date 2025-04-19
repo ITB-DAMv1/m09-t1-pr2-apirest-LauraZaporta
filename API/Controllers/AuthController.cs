d2fba1262b16abaf01a23095655f1c5aa580f441
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-19 23:51:22 +0200

    Register functions added (for users and admins)

--- API/Controllers/AuthController.cs
+++ API/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using API.Model;
+using API.DTOs;
 
 namespace API.Controllers
 {
@@ -16,6 +17,37 @@ namespace API.Controllers
             _userManager = userManager;
             _logger = logger;
             _configuration = configuration;
+        }
+
+        // Register
+        // --------
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterDTO model)
+        {
+            var usuari = new User { UserName = model.Username, Email = model.Email };
+            var resultat = await _userManager.CreateAsync(usuari, model.Password);
+
+            if (resultat.Succeeded)
+                return Ok("Usuari registrat");
+
+            return BadRequest(resultat.Errors);
+        }
+        [HttpPost("admin/register")]
+        public async Task<IActionResult> AdminRegister([FromBody] RegisterDTO model)
+        {
+            var usuari = new User { UserName = model.Username, Email = model.Email };
+            var resultat = await _userManager.CreateAsync(usuari, model.Password);
+            var resultatRol = new IdentityResult();
+
+            if (resultat.Succeeded)
+            {
+                resultatRol = await _userManager.AddToRoleAsync(usuari, "Admin");
+            }
+
+            if (resultat.Succeeded && resultatRol.Succeeded)
+                return Ok("Usuari registrat");
+
+            return BadRequest(resultat.Errors);
         }
     }
 }
