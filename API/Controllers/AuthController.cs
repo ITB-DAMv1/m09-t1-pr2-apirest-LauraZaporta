743df1e68fc131d677744b4b21e29d14c87b4d55
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-19 23:47:06 +0200

    Initial setup for auth controller added. Fisrt migration done.

---
+++ API/Controllers/AuthController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using API.Model;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly UserManager<User> _userManager;
+        private readonly ILogger<AuthController> _logger;
+        private readonly IConfiguration _configuration;
+        public AuthController(UserManager<User> userManager, ILogger<AuthController> logger, IConfiguration configuration)
+        {
+            _userManager = userManager;
+            _logger = logger;
+            _configuration = configuration;
+        }
+    }
+}
