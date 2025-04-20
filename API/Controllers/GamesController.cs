8a0b5c97f0e96ccc6567aa2f982e6d6f4b3a54ad
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-20 07:58:31 +0200

    Initialize setup for games controller

---
+++ API/Controllers/GamesController.cs
@@ -0,0 +1,16 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GamesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+        public GamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+    }
+}
