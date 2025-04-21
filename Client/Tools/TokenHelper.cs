d2d16bb310947884e025b65295a83e8ef5ba29ba
Laura Zaporta <laura.zaporta.7e8@itb.cat>
2025-04-21 23:27:12 +0200

    Added token helper

---
+++ Client/Tools/TokenHelper.cs
@@ -0,0 +1,24 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace Client.Tools
+{
+    public static class TokenHelper
+    {
+        public static bool IsTokenSession(string token)
+        {
+            return !string.IsNullOrEmpty(token) && !IsTokenExpired(token);
+        }
+        /// <summary>
+        /// Validar si el token ha expirat o no
+        /// </summary>
+        /// <param name="token">Token de sessió</param>
+        /// <returns></returns>
+        public static bool IsTokenExpired(string token)
+        {
+            var handler = new JwtSecurityTokenHandler();
+            var jwt = handler.ReadJwtToken(token);
+            var expiration = jwt.ValidTo;
+            return expiration < DateTime.UtcNow;
+        }
+    }
+}
